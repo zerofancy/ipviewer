4a0c0b048853467582d3268b969fb4ef8a4a6449
bytedance <bytedance@MacBook-Pro.local>
2020-12-20 18:08:30 +0800

    format

--- app/src/main/java/top/ntutn/ipviewer/IPAdapter.kt
+++ app/src/main/java/top/ntutn/ipviewer/IPAdapter.kt
@@ -9,21 +9,21 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.ip_item.view.*
 
 class IPAdapter(ipList: List<String>) : RecyclerView.Adapter<IPAdapter.IPViewHolder>() {
-    var ipList= listOf<String>()
+    var ipList = listOf<String>()
         set(value) {
-            DiffUtil.calculateDiff(IPDiffCallback(field,value))
-            field=value
+            DiffUtil.calculateDiff(IPDiffCallback(field, value))
+            field = value
         }
 
     init {
-        this.ipList=ipList
+        this.ipList = ipList
     }
 
     class IPViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IPViewHolder {
         val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.ip_item, parent, false)
+            .inflate(R.layout.ip_item, parent, false)
         return IPViewHolder(view)
     }
 
@@ -35,12 +35,15 @@ class IPAdapter(ipList: List<String>) : RecyclerView.Adapter<IPAdapter.IPViewHol
     override fun getItemCount() = ipList.size
 }
 
-class IPDiffCallback(val oldIPList: List<String>, val newIPList: List<String>) : DiffUtil.Callback() {
+class IPDiffCallback(val oldIPList: List<String>, val newIPList: List<String>) :
+    DiffUtil.Callback() {
     override fun getOldListSize() = oldIPList.size
 
     override fun getNewListSize() = newIPList.size
 
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldIPList[oldItemPosition] == newIPList[newItemPosition]
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        oldIPList[oldItemPosition] == newIPList[newItemPosition]
 
-    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldIPList[oldItemPosition] == newIPList[newItemPosition]
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        oldIPList[oldItemPosition] == newIPList[newItemPosition]
 }
